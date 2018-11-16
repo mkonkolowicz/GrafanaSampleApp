322b93f9eae66e56f9b858f7b71e87ed62ffbdc8
Maciek Konkolowicz <maciekkonkolowicz@amrock.com>
2018-11-16 13:58:20 -0500

    New project added

---
+++ ToriFirstApp/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToriSampleApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult OurFamily()
+        {
+            ViewData["Message"] = "Maciek, Brandi and Victoria";
+
+            return View();
+        }
+    }
+}
