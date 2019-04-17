9761a7683f8abff7bb45be3e9aacc0a8455ea7b1
Maciek Konkolowicz <mkonkolowicz@gmail.com>
2019-04-17 10:27:00 -0400

    Updated computer name

--- ToriFirstApp/Controllers/HomeController.cs
+++ ToriFirstApp/Controllers/HomeController.cs
@@ -21,7 +21,7 @@ namespace ToriSampleApp.Controllers
 
             var telemetryClient = new TelemetryClient();
             telemetryClient.Context.User.Id = "mkonkolowicz";
-            telemetryClient.Context.Device.Id = "57L8HR2";
+            telemetryClient.Context.Device.Id = "9FCLVT2";
 
             var social = new Dictionary<string, string>() { { "ssn", "123-45-6789" } };
             telemetryClient.TrackEvent("Social", social);
